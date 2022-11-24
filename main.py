90cdb3f34250598d024f766a1fb40f8905ca78ec
L3XxXa <alexkkk@inbox.ru>
2022-11-25 00:16:29 +0700

    Commit

---
+++ main.py
@@ -0,0 +1,16 @@
+from telethon import TelegramClient
+import random
+api_id = int(input("Input api_id\n"))
+api_hash = input("Input api_hash\n")
+channel_name = input("Input channel link")
+users_list = []
+with TelegramClient('anon', api_id, api_hash) as client:
+    user_name = client.loop.run_until_complete(client.get_me()).username
+    users = client.loop.run_until_complete(client.get_participants(channel_name))
+    for i in range(len(users)):
+            users_list.append(users[i].username)
+    users_list.remove(user_name)
+    random.shuffle(users_list)
+    winner = users_list[0]
+    client.loop.run_until_complete(
+        client.send_message(channel_name, f"И ПОБЕДИТЕЛЬ РОЗЫГРЫША - @{winner}. НАПИШИ МНЕ ПЫЖЫ, КСТА, ПОЗДРАВЛЯЮ С ПОБЕДОЙ")) #Change the text, but don't forget about var winner
---
+++ readme.md
@@ -0,0 +1,13 @@
+## Program for holding draws among Telegram channel subscribers
+
+```
+cd existing_repo
+git clone https://github.com/L3XxXa/simple_draws
+pip3 install -r requirements.txt
+python main.py
+```
+### Required
+- Python 3
+- Telegram account and 
+- API_ID and API_HASH (to get them [visit](https://my.telegram.org/))
+- Telethon (will be installed)
